0cbbd6a96b41411ce068802dfa39bf36c9c38506
Taulant Ibraj <tibraj94@gmail.com>
2020-03-24 15:54:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,3 +32,11 @@ def position_taken?(board, index)
     return true
   end
 end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
