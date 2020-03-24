af4ce3056b8d37c280247098cd84e406c39133ea
Taulant Ibraj <tibraj94@gmail.com>
2020-03-24 15:56:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,10 +52,12 @@ def turn(board)
   end
 end
 
-def play(board)
+def turn_count(board)
   counter = 0
-  while counter < 9
-    turn(board)
-    counter += 1
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
   end
+  counter
 end
